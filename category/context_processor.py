43bd80f0e57b2178e4377b26424a7108ab9d80ea
Лычагин Сергей <serg.programmer777@gmail.com>
2023-01-26 11:08:19 +0300

    Add some changes in home.html

--- category/context_processor.py
+++ category/context_processor.py
@@ -7,6 +7,6 @@ def menu_links(request):
     Get links for all categories in database.
     After that we can use this links in templates anywhere in the project.
     """
-    category_links = Category.objects.all().order_by('category_name')
-    product_links = [Product.objects.all().filter(category=category).order_by('product_name') for category in category_links]
+    category_links = Category.objects.all()
+    product_links = [Product.objects.all().filter(category=category).order_by('created_date') for category in category_links]
     return dict(category_links=category_links, product_links=product_links)

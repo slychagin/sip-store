acd52316354d644d8c2b8cb525d4e86fa0c902dc
Лычагин Сергей <serg.programmer777@gmail.com>
2023-01-30 11:58:21 +0300

    Changed store and product details function views on class-dased views

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from store import views
 
 
 urlpatterns = [
-    path('', views.store, name='store'),
-    path('<slug:category_slug>/', views.store, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', views.product_details, name='product_details')
+    path('', views.StorePageView.as_view(), name='store'),
+    path('<slug:category_slug>/', views.CategoryProductsListView.as_view(), name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.ProductDetailView.as_view(), name='product_details')
 ]
--- store/views.py
+++ store/views.py
@@ -1,39 +1,70 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from category.models import Category
 from store.models import Product
 
 
-def store(request, category_slug=None):
-    """Rendering store page"""
-    categories = None
-    products = None
+class StorePageView(ListView):
+    """Rendering all products in store page"""
+    template_name = 'store/store.html'
+    queryset = Product.objects.all().filter(is_available=True)
+    context_object_name = 'products'
 
-    if category_slug is not None:
-        categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
-    else:
-        products = Product.objects.all().filter(is_available=True)
-
-    context = {
-        'products': products,
-        'product_count': products.count()
-    }
-    return render(request, 'store/store.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_count'] = context['products'].count()
+        return context
 
 
-def product_details(request, category_slug, product_slug):
-    """Rendering product details page"""
-    try:
-        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-    except Exception as e:
-        raise e
+class CategoryProductsListView(ListView):
+    """Rendering products by category in store page"""
+    template_name = 'store/store.html'
 
-    context = {
-        'single_product': single_product
-    }
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.products = None
+        self.categories = None
+        self.category_slug = None
 
-    return render(request, 'store/product_details.html', context)
+    def get_queryset(self):
+        """Return products by category"""
+        self.categories = get_object_or_404(Category, slug=self.kwargs['category_slug'])
+        self.products = Product.objects.filter(category=self.categories, is_available=True)
+        return self.products
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = self.products
+        context['product_count'] = self.products.count()
+        return context
 
 
+class ProductDetailView(DetailView):
+    """Render a single product details page"""
+    template_name = 'store/product_details.html'
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.category_slug = None
+        self.product_slug = None
+        self.single_product = None
+
+    def get_object(self, **kwargs):
+        """Return single product by category and product slugs"""
+        try:
+            self.single_product = Product.objects.get(
+                category__slug=self.kwargs['category_slug'],
+                slug=self.kwargs['product_slug']
+            )
+        except ObjectDoesNotExist:
+            raise Http404('Сторінку не знайдено')
+
+        return self.single_product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['single_product'] = self.single_product
+        return context

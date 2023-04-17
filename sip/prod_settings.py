4d7fe440188dd91556aa21b88666ed8e92cdb3f7
Лычагин Сергей <serg.programmer777@gmail.com>
2023-04-17 16:28:02 +0300

    Change settings

--- sip/prod_settings.py
+++ sip/prod_settings.py
@@ -11,7 +11,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 DATABASES = {
     'default': {
